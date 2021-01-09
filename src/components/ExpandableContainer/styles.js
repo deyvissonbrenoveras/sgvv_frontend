d0ecb45f301d9732c757c2129fd1e9733a4b3621
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2021-01-09 11:58:20 -0300

    Ajuste no container do mapa

--- src/components/ExpandableContainer/styles.js
+++ src/components/ExpandableContainer/styles.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  position: ${(props) => (props.expanded ? 'relative' : 'absolute')};
-  bottom: 20px;
+  /* position: ${(props) => (props.expanded ? 'relative' : 'absolute')}; */
+  position: absolute;
+  bottom: ${(props) => (props.expanded ? 'auto' : '20px')};
+  top: ${(props) => (props.expanded ? '0' : 'auto')};
   width: ${(props) => (props.expanded ? '100%' : '200px')};
   height: ${(props) => (props.expanded ? '100%' : '100px')};
+  left: ${(props) => (props.expanded ? '0' : '10px')};
 `;
 
 export const ExpandButton = styled.button`
@@ -13,8 +16,8 @@ export const ExpandButton = styled.button`
   position: absolute;
   bottom: ${(props) => (props.expanded ? 'auto' : '90px')};
   left: ${(props) => (props.expanded ? 'auto' : '183px')};
-  right: ${(props) => (props.expanded ? '40px' : 'auto')};
-  top: ${(props) => (props.expanded ? '20px' : 'auto')};
+  right: ${(props) => (props.expanded ? '10px' : 'auto')};
+  top: ${(props) => (props.expanded ? '10px' : 'auto')};
   border: none;
   outline: none;
 `;
