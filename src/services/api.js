92872b9263dc5a11aa46adc944aaf23634397456
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2021-01-25 14:56:35 -0300

    Alterações para deployh

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.91:3333',
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? '/api'
+      : 'http://192.168.0.91:3333/api',
 });
 
 export default api;
